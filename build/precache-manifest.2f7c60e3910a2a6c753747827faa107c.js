f63a35d75592a267b2cd6c045e44698f33514d30
AriefRahmanW <rivurjr@gmail.com>
2021-01-16 16:08:49 +0700

    add build folder

---
+++ build/precache-manifest.2f7c60e3910a2a6c753747827faa107c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b1db3b20d550bf2f99df7ca238adcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3895d65d6b5b611acc0e",
+    "url": "/static/css/main.f3a01090.chunk.css"
+  },
+  {
+    "revision": "3fc86e93dc1808e43e32",
+    "url": "/static/js/2.a9448f43.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/static/js/2.a9448f43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3895d65d6b5b611acc0e",
+    "url": "/static/js/main.c441f18b.chunk.js"
+  },
+  {
+    "revision": "1b1fee1ec2f8c2fb8cf1",
+    "url": "/static/js/runtime-main.7fa4ae1c.js"
+  },
+  {
+    "revision": "d539b318a6d3bd80426fad5e3b787ae5",
+    "url": "/static/media/RISET-MOBILE-ROBOT.d539b318.png"
+  },
+  {
+    "revision": "d24988f24e362aae004716a66e7b7891",
+    "url": "/static/media/RISET-ROV.d24988f2.png"
+  },
+  {
+    "revision": "bc860d47a61e51ee3f9284998d75331a",
+    "url": "/static/media/RISET-UAV.bc860d47.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "c8095b051ff851483be429a061f47262",
+    "url": "/static/media/illustration-section-01.c8095b05.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "9fe8474fad5aa7cda25d3dfdab1395b9",
+    "url": "/static/media/rbt.9fe8474f.jpg"
+  },
+  {
+    "revision": "ac48e30c74ffd31b503aedd7c3c2f19f",
+    "url": "/static/media/robotika.ac48e30c.png"
+  }
+]);
